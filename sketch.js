c813d5bb41aea365ff43841ec2fc39c54795139b
RashadKhaled <rashadtajribk@gmail.com>
2020-09-05 15:02:00 -0400

    fixed the bug

--- sketch.js
+++ sketch.js
@@ -46,7 +46,7 @@ function draw() {
 }
 function keyPressed(){
   if(keyCode===UP_ARROW){
-    Matter.Body.applyForce(paper1.body,paper1.body.position,{x:100,y:-20});
+    Matter.Body.applyForce(paper1.body,paper1.body.position,{x:250,y:-800});
   }
 }
 
